25d8ad97c877882464f2bc1768c1d9b77e97fffb
Arnau Carné Viadé <acarneviade@gmail.com>
2024-03-06 17:07:17 +0100

    hem agregat al classe Leerarchivo dins el project_code2 general

---
+++ LeerArchivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2_Code
+{
+
+    public class LeerArchivo
+    {
+        public LeerArchivo(string file)
+        {
+         
+        }
+
+    }
+}
