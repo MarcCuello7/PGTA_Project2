cd0bcaf65b130c965afc6596cafa19238645dfc3
Marc Cuello <marckitus20@gmail.com>
2024-04-10 00:24:20 +0200

    First big advance

---
+++ CAT48.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+namespace Project2_Code
+{
+    public class CAT48
+    {
+        public BitArray FSPEC;
+
+        //Data Item I048/010, Data Source Identifier
+        public int SAC;
+        public int SIC;
+
+        //Data Item I048/020, Target Report Descriptor
+        public string TYP_020;
+        public string SIM_020;
+        public string RDP_020;
+        public string SPI_020;
+        public string RAB_020;
+        public string TST_020;
+        public string ERR_020;
+        public string XPP_020;
+        public string ME_020;
+        public string MI_020;
+        public string FOEFRI_020;
+        public string ADSB_020;
+        public string SCN_020;
+        public string PAI_020;
+
+        //Data Item I048/030, Warning/Error Conditions and Target Classification
+        //No decodification needed
+
+        //Data Item I048/040, Measured Position in Polar Co-ordinates
+        public string RHO;
+        public string THETA;
+
+        //Data Item I048/042, Calculated Position in Cartesian Co-ordinates
+        public string LAT;
+        public string LONG;
+
+        //Data Item I048/050, Mode-2 Code in Octal Representation
+        //No decodification needed
+
+        //Data Item I048/055, Mode-1 Code in Octal Representation
+        //No decodification needed
+
+        //Data Item I048/060, Mode-2 Code Confidence Indicator
+        //No decodification needed
+
+        //Data Item I048/065, Mode-1 Code Confidence Indicator
+        //No decodification needed
+
+        //Data Item I048/070, Mode-3/A Code in Octal Representation
+        public string V_070;
+        public string G_070;
+        public string L_070;
+        public string MODE3AREPLY;
+
+        //Data Item I048/080, Mode-3/A Code Confidence Indicator
+        //No decodification needed
+
+        //Data Item I048/090, Flight Level in Binary Representation
+        public string V_090;
+        public string G_090;
+        public string FL;
+
+        //Data Item I048/100, Mode-C Code and Code Confidence Indicator
+        //No decodification needed
+
+        //Data Item I048/110, Height Measured by a 3D Radar
+        public string HEIGH3D;
+
+        //Data Item I048/120, Radial Doppler Speed
+        //No decodification needed
+
+        //Data Item I048/130, Radar Plot Characteristics
+        public string SRL_130;
+        public string SRR_130;
+        public string SAM_130;
+        public string PRL_130;
+        public string PAM_130;
+        public string RPD_130;
+        public string APD_130;
+
+        //Data Item I048/140, Time of Day
+        public string TIME;
+
+        //Data Item I048/161, Track Number
+        public string TN;
+
+        //Data Item I048/170, Track Status
+        public string CNF_170;
+        public string RAD_170;
+        public string DOU_170;
+        public string MAH_170;
+        public string CDM_170;
+        public string TRE_170;
+        public string GHO_170;
+        public string SUP_170;
+        public string TCC_170;
+
+        //Data Item I048/200, Calculated Track Velocity in Polar Co-ordinates
+        public string GS;
+        public string HEADING;
+
+
+        //Enumeration of the category 48 Standard User Application Profile
+        enum UAP 
+        {
+            I048_010 = 0,   // Data Source Identifier
+            I048_140,       // Time-of-Day
+            I048_020,       // Target Report Descriptor
+            I048_040,       // Measured Position in Slant Polar Coordinates
+            I048_070,       // Mode-3/A Code in Octal Representation
+            I048_090,       // Flight Level in Binary Representation
+            I048_130,       // Radar Plot Characteristics
+                            // Field Extension Indicator
+            I048_220 = 8,   // Aircraft Address
+            I048_240,       // Aircraft Identification
+            I048_250,       // Mode S MB Data
+            I048_161,       // Track Number
+            I048_042,       // Calculated Position in Cartesian Coordinates
+            I048_200,       // Calculated Track Velocity in Polar Representation
+            I048_170,       // Track Status
+                            // Field Extension Indicator
+            I048_210 = 16,  // Track Quality
+            I048_030,       // Warning/Error Conditions/Target Classification
+            I048_080,       // Mode-3/A Code Confidence Indicator
+            I048_100,       // Mode-C Code and Confidence Indicator
+            I048_110,       // Height Measured by 3D Radar
+            I048_120,       // Radial Doppler Speed
+            I048_230,       // Communications / ACAS Capability and Flight Status
+                            // Field Extension Indicator
+            I048_260 = 24,  // ACAS Resolution Advisory Report
+            I048_055,       // Mode-1 Code in Octal Representation
+            I048_050,       // Mode-2 Code in Octal Representation
+            I048_065,       // Mode-1 Code Confidence Indicator
+            I048_060,       // Mode-2 Code Confidence Indicator
+            SP_DI,          // Item Special Purpose Field
+            RE_DI,          // Item Reserved Expansion Field
+        }
+
+
+        //METHODS
+        private int ParseFSPEC(Stream data)
+        {
+            int length = 0;
+            byte[] FSPEC = new byte[] {0, 0, 0, 0};
+            for (int i = 0; i < FSPEC.Length; i++)
+            {
+                FSPEC[i] = data[i];
+                if ((data[i] & 1) == 0)
+                {
+                    length = i + 1;
+                    break;
+                }            
+            }
+            this.FSPEC = new BitArray(FSPEC);
+            return length;
+        }
+
+        private void ParseI048_010(Stream data)
+        {
+            this.SAC = (int)data.ReadByte();
+            this.SIC = (int)data.ReadByte();
+        }
+
+        //CONSTRUCTOR
+        public CAT48(Stream data)
+        {
+            int pos = 0;
+            pos += ParseFSPEC(data[pos..(pos+4)]);
+
+            //Check
+          
+            if (this.FSPEC[(int)UAP.I048_010])
+            {
+                ParseI048_010
+                
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Globalization;
+
+namespace Project2_Code
+{
+    class Utils
+    {
+        /// <summary>
+        /// The base Kaitai stream which exposes an API for the Kaitai Struct framework.
+        /// It's based off a <code>BinaryReader</code>, which is a little-endian reader.
+        /// <summary>
+        /// Read a signed byte from the stream
+        /// </summary>
+        /// <returns></returns>
+        public sbyte ReadS1(Stream data)
+        {
+            return ReadSByte();
+        }
+
+
+        /// <summary>
+        /// Read a signed short from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public short ReadS2be(Stream data)
+        {
+            return BitConverter.ToInt16(ReadBytes(2), 0);
+        }
+
+        /// <summary>
+        /// Read a signed int from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public int ReadS4be(Stream data)
+        {
+            return BitConverter.ToInt32(ReadBytes(4), 0);
+        }
+
+        /// <summary>
+        /// Read a signed long from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public long ReadS8be(Stream data)
+        {
+            return BitConverter.ToInt64(ReadBytes(8), 0);
+        }
+
+
+        /// <summary>
+        /// Read an unsigned byte from the stream
+        /// </summary>
+        /// <returns></returns>
+        public byte ReadU1(Stream data)
+        {
+            return ReadByte();
+        }
+
+
+
+        /// <summary>
+        /// Read an unsigned short from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public ushort ReadU2(Stream data)
+        {
+            return BitConverter.ToUInt16(ReadBytes(2), 0);
+        }
+
+        /// <summary>
+        /// Read an unsigned int from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public uint ReadU4(Stream data)
+        {
+            return BitConverter.ToUInt32(ReadBytes(4), 0);
+        }
+
+        /// <summary>
+        /// Read an unsigned long from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public ulong ReadU8(Stream data)
+        {
+            return BitConverter.ToUInt64(ReadBytes(8), 0);
+        }
+
+  
+
+        /// <summary>
+        /// Read a single-precision floating point value from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public float ReadF4(Stream data)
+        {
+            return BitConverter.ToSingle(ReadBytes(4), 0);
+        }
+
+        /// <summary>
+        /// Read a double-precision floating point value from the stream (big endian)
+        /// </summary>
+        /// <returns></returns>
+        public double ReadF8(Stream data)
+        {
+            return BitConverter.ToDouble(data.ReadBytes(8), 0);
+        }
+
+      
+        /// <summary>
+        /// Read a n-bit integer in a big-endian manner from the stream
+        /// </summary>
+        /// <returns></returns>
+        public ulong ReadBitsInt(int n)
+        {
+            ulong res = 0;
+
+            int bitsNeeded = n - BitsLeft;
+            BitsLeft = -bitsNeeded & 7; // `-bitsNeeded mod 8`
+
+            if (bitsNeeded > 0)
+            {
+                // 1 bit  => 1 byte
+                // 8 bits => 1 byte
+                // 9 bits => 2 bytes
+                int bytesNeeded = ((bitsNeeded - 1) / 8) + 1; // `ceil(bitsNeeded / 8)`
+                byte[] buf = ReadBytes(bytesNeeded);
+                for (int i = 0; i < bytesNeeded; i++)
+                {
+                    res = res << 8 | buf[i];
+                }
+
+                ulong newBits = res;
+                res = res >> BitsLeft | Bits << bitsNeeded;
+                Bits = newBits; // will be masked at the end of the function
+            }
+            else
+            {
+                res = Bits >> -bitsNeeded; // shift unneeded bits out
+            }
+
+            ulong mask = (1UL << BitsLeft) - 1; // `BitsLeft` is in range 0..7, so `(1UL << 64)` does not have to be considered
+            Bits &= mask;
+
+            return res;
+        }
+
+        [Obsolete("use ReadBitsIntBe instead")]
+        public ulong ReadBitsInt(int n)
+        {
+            return ReadBitsIntBe(n);
+        }   
+    }
+}
